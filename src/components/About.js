046e974866499a92b1b9f92b1ef7603d93944076
Chukwudi Eze <chukwudieze97@gmail.com>
2020-09-28 17:40:58 +0100

    some adjustments

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AboutImage from '../img/undraw_dashboard_nklg.svg'
 
 function About() {
+    const [read, setRead] = useState(false)
+    const handleRead = () => {
+        if (read) {
+            setRead(false)
+        } else {
+            setRead(true)
+        }
+    } 
     return (
         <section className="about border-bottom pb-5" id="about">
           <div className="section-heading text-center pb-3" data-aos="fade-up">
@@ -18,11 +26,16 @@ function About() {
               </div>
               <div className="col-lg-6 my-auto">
                   <div className="about-content mx-auto text-dark px-3 py-5" data-aos="zoom-out">
-                      <p><b><span className="text-logo">EASY</span>CRM</b> is a customer relations management system targeted towards small scale businesses. It helps you boost sales by keeping an eye on customers, from nurturing leads to closing the sale and maintaining customer loyalty. 
-                      It will save you time and money through sales force automation. <br /><br />
-                      <b><span className="text-logo">EASY</span>CRM</b> is therefore the one-stop shop for your sales teams. 
-                      It can also be your gateway to increased sales productivity, healthier pipelines and better coordination between (and beyond) teams.
-                     </p>
+                      <p><b><span className="text-logo">EASY</span>CRM</b> is a customer relations management system targeted towards small scale businesses. It helps you boost sales by keeping an eye on customers, from nurturing leads to closing the sale and maintaining customer loyalty.
+                      
+
+                      {
+                          read ? <span>It will save you time and money through sales force automation. <br /><br /><b><span className=" text-logo">EASY</span>CRM</b> is therefore the one-stop shop for your sales teams. 
+                        It can also be your gateway to increased sales productivity, healthier pipelines and better coordination between (and beyond) teams.</span>
+                         : ''
+                      }
+                      </p>
+                      <span onClick={handleRead} className="text-warning" style={{cursor: 'spanointer'}}>Read More</span>
                   </div>
               </div>
           </div>
