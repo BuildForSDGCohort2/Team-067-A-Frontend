d0a088503d5bf7362266c2473933a8ba1c3f0c07
Chukwudi Eze <chukwudieze97@gmail.com>
2020-09-28 16:24:31 +0100

    made changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <div className="main-menu">
             <nav data-aos="fade-down" className="navbar navbar-expand-sm navbar-dark container">
-                <a className="navbar-brand text-white font-weight-bold" href="/"><img  alt='logo' src='https://res.cloudinary.com/moversng/image/upload/v1598887746/easycrmx2_kqmi0r.svg' className='logo'/></a>
+                <a className="navbar-brand text-white font-weight-bold" href="/"><img  alt='logo' src='https://res.cloudinary.com/moversng/image/upload/v1598887746/easycrmx2_kqmi0r.svg' className='logo border-red'/></a>
                 <button className="navbar-toggler d-lg-none text-white menu-btn" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                     aria-expanded="false" aria-label="Toggle navigation">Menu</button>
                 <div className="collapse navbar-collapse" id="collapsibleNavId">
