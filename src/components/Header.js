462aba1b815f2739ec3891b5c4bc9d5b1164932a
Chukwudi Eze <chukwudieze97@gmail.com>
2020-09-30 11:37:00 +0100

    fix respnsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ function Header() {
         <header className="header-container">
             <div className="container pb-5 header-content-container">
                 <div className="row">
-                    <div className="col-lg-5 my-auto">
+                    <div className="col-lg-5 col-md-7 my-auto">
                         <div className="header-content mx-auto text-white" >
                             <h2 data-aos="fade-down-right" className="mb-5">Manage customer data  and grow your business efficiently with <img alt='logo' src='https://res.cloudinary.com/moversng/image/upload/v1598887746/easycrmx2_kqmi0r.svg' className='logo'/></h2>
                             <a href="https://sdgcrm.herokuapp.com/signup" rel="noopener noreferrer" target={"_blank"} className="btn btn-outline-warning btn-xl js-scroll-trigger p-2">Sign Up for Free!</a>
                         </div>
                     </div>
-                    <div className="col-lg-7 my-auto">
+                    <div className="col-lg-7 col-md-5 my-auto">
                         <div className="device-container">
                             <div className="device" data-aos="zoom-in">
                                 <img src={HeaderImage}  alt='headerImg'/>
