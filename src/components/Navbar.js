fcdd06eea14cc46fa173e77b5dd8308c0b54e514
Chukwudi Eze <chukwudieze97@gmail.com>
2020-09-30 11:15:29 +0100

    fixed an issue with the menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink, Link } from 'react-router-dom'
 
 function Navbar() {
+    useEffect(()=>{
+        let link = document.querySelector('.nav-link');
+        let menu = document.querySelector('.navbar-toggler');
+        link.addEventListener('click', function() {
+            menu.click();
+        });
+        window.addEventListener('click', function() {
+            if (menu.getAttribute('aria-expanded') === "true") {
+                menu.click();
+            }
+        })
+    }, []);
     return (
         <div className="main-menu">
             <nav data-aos="fade-down" className="navbar navbar-expand-sm navbar-dark container">
                 <Link className="navbar-brand text-white font-weight-bold" to="/"><img  alt='logo' src='https://res.cloudinary.com/moversng/image/upload/v1598887746/easycrmx2_kqmi0r.svg' className='logo border-red'/></Link>
                 <button className="navbar-toggler d-lg-none text-white menu-btn" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation">Menu</button>
+                    aria-expanded="false" aria-label="Toggle navigation">
+                        <span className='icon-bar'></span>
+                        <span className='icon-bar'></span>
+                        <span className='icon-bar'></span>
+                    </button>
                 <div className="collapse navbar-collapse" id="collapsibleNavId">
                     <ul className="navbar-nav ml-auto mt-lg-0 z4 font-weight-bold">
                         <li className="nav-item">
