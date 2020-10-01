7ced61cb9d867c33eb9d250a8fa5f6cadbb49064
Chukwudi Eze <chukwudieze97@gmail.com>
2020-10-01 13:47:35 +0100

    css

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
                 <div className="row">
                     <div className="col-lg-5 col-md-7 my-auto">
                         <div className="header-content text-white" >
-                            <h2 data-aos="fade-down-right" className="mb-5">Manage customer data  and grow your business efficiently with <img alt='logo' src='https://res.cloudinary.com/moversng/image/upload/v1598887746/easycrmx2_kqmi0r.svg' className='logo'/></h2>
+                            <h2 data-aos="fade-down-left" className="mb-5">Manage customer data  and grow your business efficiently with <img alt='logo' src='https://res.cloudinary.com/moversng/image/upload/v1598887746/easycrmx2_kqmi0r.svg' className='logo'/></h2>
                             <a href="https://sdgcrm.herokuapp.com/signup" rel="noopener noreferrer" target={"_blank"} className="btn btn-outline-warning btn-xl js-scroll-trigger p-2">Sign Up for Free!</a>
                         </div>
                     </div>
